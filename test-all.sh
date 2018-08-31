ea4757356f78b0fcfbe0a9aed3874d372c8cc5aa
Nigel Deakin <nigel.deakin@oracle.com>
2018-08-31 10:29:22 +0100

    Run rdd

--- test-all.sh
+++ test-all.sh
@@ -110,6 +110,8 @@ testScratchPush () {
 
 runTests() {
 
+  source $testsDir/rdd/test.sh || return 1
+
   #  The following tests must be skipped when run in a wercker pipeline 
   if [ -z ${WERCKER_ROOT} ]; then 
     # The rdd tests cannot be run in wercker because the pipeline cannot connect to the daemon
